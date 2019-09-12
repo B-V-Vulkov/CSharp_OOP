00ab25aabf7e3030d7aaef52ef0f435586d41d10
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-12 15:08:42 +0300

    Add P01. Person

---
+++ Exercises/03_Inheritance/Solutions/Person/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Person
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Person person = new Person(name, age);
+            Console.WriteLine(person);
+        }
+    }
+}
