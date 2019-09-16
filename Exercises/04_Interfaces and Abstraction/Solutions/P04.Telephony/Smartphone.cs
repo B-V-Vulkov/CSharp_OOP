e14dbcac48c0bb3c7022dfdcebfd10f43892c457
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-09-16 11:30:37 +0300

    Add P04. Telephony

---
+++ Exercises/04_Interfaces and Abstraction/Solutions/P04.Telephony/Smartphone.cs
@@ -0,0 +1,66 @@
+﻿namespace P04.Telephony
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Smartphone : IBrowsable, ICallable
+    {
+        public List<string> Sites { get; }
+        public List<string> Numbers { get; }
+
+        public Smartphone()
+        {
+            this.Sites = new List<string>();
+            this.Numbers = new List<string>();
+        }
+
+        public void AddSite(string site)
+        {
+            if (site.Any(char.IsNumber))
+            {
+                this.Sites.Add("Invalid URL!");
+            }
+            else
+            {
+                this.Sites.Add($"Browsing: {site}!");
+            }
+        }
+
+        public void AddNumber(string number)
+        {
+            if (!number.All(char.IsDigit))
+            {
+                this.Numbers.Add("Invalid number!");
+            }
+            else
+            {
+                this.Numbers.Add($"Calling... {number}");
+            }
+        }
+
+        public string Browsing()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var site in this.Sites)
+            {
+                stringBuilder.AppendLine(site);
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+
+        public string Call()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var number in this.Numbers)
+            {
+                stringBuilder.AppendLine(number);
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Exercises/04_Interfaces and Abstraction/Solutions/P04.Telephony/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace P04.Telephony
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] numbers = Console.ReadLine()
+                .Split(" ");
+
+            string[] sites = Console.ReadLine()
+                .Split(" ");
+
+            Smartphone smartphone = new Smartphone();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                smartphone.AddNumber(numbers[i]);
+            }
+
+            for (int i = 0; i < sites.Length; i++)
+            {
+                smartphone.AddSite(sites[i]);
+            }
+
+            Console.WriteLine(smartphone.Call());
+            Console.WriteLine(smartphone.Browsing());
+        }
+    }
+}
